08d0a7d084cc900f1bc3482c9a1552ae0b116e01
Kulkarni Vishal <88853198+Vishalk29@users.noreply.github.com>
2025-10-03 00:12:56 +0530

    dashboard layout is done

--- frontend/app/types/index.ts
+++ frontend/app/types/index.ts
@@ -3,4 +3,21 @@ export interface User {
   email: string;
   name: string;
   createdAt: Date;
+  updatedAt: Date;
+  profilePicture?: string;
+}
+
+export interface Workspace {
+  _id: string;
+  name: string;
+  description?: string;
+  owner: User | string;
+  color: string;
+  members: {
+    user: User;
+    role: "admin" | "member" | "owner" | "viewer";
+    joinedAt: Date;
+  }[];
+  createdAt: Date;
+  updatedAt: Date;
 }
